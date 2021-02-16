c37d18f6eca50fe76d9b928230a5dcc1056b090e
LeBorgneAntoine <leborgne2156@gmail.com>
2021-02-16 14:41:58 +0100

    nav bar test2

--- js/controllers/controller.js
+++ js/controllers/controller.js
@@ -1,12 +1,5 @@
 function displayMore(){
-
-
     let option = document.getElementById("more-option")
-    console.log(option.style.visibility)
-    if(option.style.visibility === "visible"){
-        option.style.visibility = "hidden"
-    }else{
-        option.style.visibility = "visible"
-    }
 
+    option.style.display = (option.style.display === "block")?"none":"block"
 }
